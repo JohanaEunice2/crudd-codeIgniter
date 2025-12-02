30648fa7a96f86dbc8536581de25ced559bfc1e7
JohanaEunice2 <abellana.johanaeunice.01302003@gmail.com>
2025-12-02 12:43:25 +0800

    Refactor Booking_model and add insert_booking method
    Updated the Booking_model class to improve structure and added a new insert_booking method.

--- lordHair/application/models/Booking_model.php
+++ lordHair/application/models/Booking_model.php
@@ -1,31 +1,23 @@
 <?php
-
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Booking_model extends CI_Model {
 
-    public function __construct() {
-    
+    public function __construct()
+    {
+        parent::__construct();
         $this->load->database();
     }
 
-    public function create_booking($data) {
-        $this->db->insert('bookings', $data);
-        return $this->db->insert_id();
-    }
-
-    public function get_all_bookings() {
-        $query = $this->db->get('bookings');
-        return $query->result_array();
-    }
-
-    public function get_booking_by_id($id) {
-        $this->db->where('id', $id);
-        $query = $this->db->get('bookings');
-        return $query->row_array();
-    }
-
-    public function update_booking_status($id, $status) {
-        $this->db->where('id', $id);
-        return $this->db->update('bookings', array('status' => $status));
+    /**
+     * Inserts booking data into the 'bookings' table.
+     * * @param array $data An associative array of booking details.
+     * @return bool TRUE on success, FALSE on failure.
+     */
+    public function insert_booking($data)
+    {
+        // $data array should match your table columns: 
+        // name, email, phone, booking_date, booking_time
+        return $this->db->insert('bookings', $data);
     }
 }
