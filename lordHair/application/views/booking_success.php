6cdc9f7729ceced102d582bbd6ec1529ea6527a6
JohanaEunice2 <abellana.johanaeunice.01302003@gmail.com>
2025-12-02 12:45:41 +0800

    Add files via upload

---
+++ lordHair/application/views/booking_success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Booking Confirmed</title>
+</head>
+<body>
+    <h1>Thank You!</h1>
+    <p>Your online consultation request has been received. We will contact you shortly to confirm the details.</p>
+    <a href="<?php echo base_url('booking'); ?>">Book Another Consultation</a>
+</body>
+</html>
